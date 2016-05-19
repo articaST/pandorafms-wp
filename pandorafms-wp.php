41d42d17e1c69f77a836324c263221092a941f64
mdtrooper <tres.14159@gmail.com>
2016-05-19 13:06:41 +0200

    Added a admin directory as say some 'best practices wordpress plugin development'

--- pandorafms-wp.php
+++ pandorafms-wp.php
@@ -1,2 +1,19 @@
 <?php
+/**
+ * @package PandoraFMS WP
+ * @version 0.0
+ */
+/*
+Plugin Name: PandoraFMS WP
+Plugin URI: https://github.com/articaST/pandorafms-wp
+Description: Hardening, monitoring and security plugin for Wordpress.
+Author: Miguel de Dios
+Version: 0.0
+Author URI: https://github.com/mdtrooper
+Text Domain: pandorafms-wp
+License: AGPLv3
+Copyright: (c) 2016-2016 Artica Soluciones Tecnologicas
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
