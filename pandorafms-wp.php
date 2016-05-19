46550f4f359414dd2899d488d36b5b56a98c42cd
mdtrooper <tres.14159@gmail.com>
2016-05-19 13:15:41 +0200

    Maybe there will be another developers in this plugin.

--- pandorafms-wp.php
+++ pandorafms-wp.php
@@ -7,9 +7,9 @@
 Plugin Name: PandoraFMS WP
 Plugin URI: https://github.com/articaST/pandorafms-wp
 Description: Hardening, monitoring and security plugin for Wordpress.
-Author: Miguel de Dios
+Author: Artica Soluciones Tecnologicas
 Version: 0.0
-Author URI: https://github.com/mdtrooper
+Author URI: http://artica.es/
 Text Domain: pandorafms-wp
 License: AGPLv3
 Copyright: (c) 2016-2016 Artica Soluciones Tecnologicas
